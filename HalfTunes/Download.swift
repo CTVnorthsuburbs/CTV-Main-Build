f7c9b5c28be66e117d169fc9c5cbb82dfc2781ad
William <wogura@me.com>
2016-07-15 08:47:18 -0500

    Initial Commit

---
+++ HalfTunes/Download.swift
@@ -0,0 +1,23 @@
+//
+//  Download.swift
+//  HalfTunes
+//
+//  Created by William Ogura on 7/8/16.
+//  Copyright © 2016 Ken Toh. All rights reserved.
+//
+
+import Foundation
+
+class Download: NSObject {
+    
+    var url: String
+    var isDownloading = false
+    var progress: Float = 0.0
+    
+    var downloadTask: NSURLSessionDownloadTask?
+    var resumeData: NSData?
+    
+    init(url: String) {
+        self.url = url
+    }
+}
