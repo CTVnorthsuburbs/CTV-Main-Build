836ad283714a27c1ea1b36764ce3f5000e81e8e4
William <wogura@me.com>
2017-01-04 09:57:17 -0600

    Just cleaning up before beginning work on keeping Featured Videos section loaded into memory at all times for faster access.

--- HalfTunes/MainTableViewController.swift
+++ HalfTunes/MainTableViewController.swift
@@ -17,10 +17,8 @@ var suggestedSearch : Section?
 
 class MainTableViewController: UITableViewController {
     
+    var parentCategory = featuredCategory
     
-    
-    var category1 = featuredCategory
-
     @IBOutlet weak var slideShowView: UIView!
     
     @IBOutlet weak var mainTableView: UIView!
@@ -28,88 +26,48 @@ class MainTableViewController: UITableViewController {
     @IBOutlet var tableView1: UITableView!
     
     var vc: SlideShowViewController?
-
-    
-    
     
     convenience init() {
         
         self.init()
         
+        category = self.parentCategory
         
-        
-       category = self.category1
-        
-    }
-    
-    
-    
-    
-    
-    init(category: Category) {
-        
-        
-        self.category1 = category
-        
-        
-        
-        
-       super.init(nibName: nil, bundle: nil)
-        
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        
-        
-        self.category1 = featuredCategory
-        
-        super.init(coder: aDecoder)
     }
     
     
     func setCategory(newCategory: Category) {
         
+        self.parentCategory = newCategory
         
-        
-        self.category1 = newCategory
-        
-        category = self.category1
-        
-        
+        category = self.parentCategory
         
         self.setSliderImages()
         
         self.setSlides()
         
-       
-        
     }
     
-
+    
     
     
     override func viewDidLoad() {
         
         super.viewDidLoad()
-
-       
+        
         slideShowView.frame.size.height = slideShowView.frame.width / 2.36
         
-        
-
         DispatchQueue.global(qos: .background).async {
             
             
             if(categoriesVideos.count == 0) {
-           self.generateCategories()
-            
-     
+                
+                self.generateCategories()
+                
                 
             }
         }
-
-    
-
+        
     }
     
     
@@ -139,18 +97,15 @@ class MainTableViewController: UITableViewController {
         }
         
         
-
-        
-        
-        
     }
     
     
     func setSlides() {
         
         
-        if(category1.slider?.slides != nil) {
-        vc?.setSlides(slides: (self.category1.slider?.slides)!)
+        if(parentCategory.slider?.slides != nil) {
+            
+            vc?.setSlides(slides: (self.parentCategory.slider?.slides)!)
             
         }
         
@@ -159,67 +114,25 @@ class MainTableViewController: UITableViewController {
     
     func setSliderImages() {
         
-        var slider = self.category1.getSlider()
-       
-        
-        
-        
-     
-      
+        var slider = self.parentCategory.getSlider()
         
         if(slider != nil) {
             
-     
-            
-            
-        if((slider!.images.count) > 0) {
-            
-     
-            vc?.setSlider(slider: slider!)
-            
-            
+            if((slider!.images.count) > 0) {
+                
+                vc?.setSlider(slider: slider!)
+                
+            }
             
         }
-        }
-        
         
     }
     
-    
-    
-    
     override func viewWillAppear(_ animated: Bool) {
         
+        category = self.parentCategory
         
-        
-        category = self.category1
-        
-        
-        
-        
-        /*
-
-        if (featured == true) {
-            
-            
-            category = featuredCategory
-            featured = false
-            
-            
-            
-        } else {
-            
-           //previousCategory = featuredCategory
-            featured = true
-           // previousCategory = featuredCategory
-            
-        }
-        
-*/
-
-     self.title = self.category1.categoryTitle
-        
-
+        self.title = self.parentCategory.categoryTitle
         
     }
     
@@ -229,93 +142,83 @@ class MainTableViewController: UITableViewController {
         
         self.setSlides()
         
-     
-        
     }
     
-
-  
     override func viewDidDisappear(_ animated: Bool) {
         
         /*
-
-        featured = false
+         
+         featured = false
+         
+         if(previousCategory?.categoryTitle == featuredCategory.categoryTitle) {
+         
+         featured = true
+         
+         
+         }
+         
+         previousCategory = category
+         
+         
+         
+         */
         
-        if(previousCategory?.categoryTitle == featuredCategory.categoryTitle) {
-            
-            featured = true
-            
-            
-        }
-        
-        previousCategory = category
- 
- 
- 
- */
-     
     }
     
-
-
     func categoryPressed() {
         
         self.performSegue(withIdentifier: "categoryPressed", sender: self)
         
-        
     }
     
     func changeSize(height: Int) {
-
         
-         mainTableView.frame.size.height = CGFloat(height)
-
-            self.tableView1.layoutMarginsDidChange()
-
+        mainTableView.frame.size.height = CGFloat(height)
+        
+        self.tableView1.layoutMarginsDidChange()
+        
         self.tableView1.reloadData()
         
-     self.tableView.scrollRectToVisible(CGRect(x: 0, y: 0, width: 1, height: 1), animated: true)
-
+        self.tableView.scrollRectToVisible(CGRect(x: 0, y: 0, width: 1, height: 1), animated: true)
+        
         if(vc != nil) {
             
-            
             vc?.resetSlidePosition()
+            
         }
-
+        
     }
-    
-
-
 
     override func didReceiveMemoryWarning() {
+        
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 0
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         return 0
     }
-
-
     
-
+    
+    
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
         
         
-
         
-   
+        
+        
         if (segue.identifier == "slideShow") {
             vc = segue.destination as! SlideShowViewController
             
@@ -323,9 +226,9 @@ class MainTableViewController: UITableViewController {
             
         }
         
-
+        
         
     }
-
-
+    
+    
 }
